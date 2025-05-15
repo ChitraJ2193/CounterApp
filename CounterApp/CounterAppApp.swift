2ca39bd0b6652102c52c3ddf44d156a8741a6c80
chitra joshy <chitra.joshy@laennec.ai>
2025-05-15 09:28:53 +0530

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Chitra Joshy on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
