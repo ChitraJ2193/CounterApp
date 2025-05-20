eea5fa1a99e4167c92db3d7a2977747e96b17f4c
chitra joshy <chitra.joshy@laennec.ai>
2025-05-20 15:43:53 +0530

    new changes new

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Counter Test")
+            Text("Counter Test New label")
                 .font(.largeTitle)
                 .fontWeight(.bold)
             
