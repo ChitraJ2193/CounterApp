8be2fceb25bc9a39f714896233609dcca5e1e162
chitra joshy <chitra.joshy@laennec.ai>
2025-05-20 14:12:17 +0530

    changed the label

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Counter")
+            Text("Counter Test")
                 .font(.largeTitle)
                 .fontWeight(.bold)
             
